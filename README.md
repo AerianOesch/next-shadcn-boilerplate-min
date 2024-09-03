f75d039f7d2fbd006a0b5cdc1ed664e942f224e3
AerianOesch <aerianthomas@gmail.com>
2024-09-03 12:41:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# next-template
+
+A Next.js 14 template for building apps with Shadcn/ui and Tailwind CSS.
+
+## Features
+
+- Next.js 14 App Directory
+- Dark mode with `next-themes`
+- TypeScript
+
+## License
+
+Licensed under the [MIT license].
---
+++ config/site.ts
@@ -0,0 +1,18 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Next shadcn/ui",
+  description:
+    "Nextjs shadcn/ui boilerplate",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "/",
+    github: "/",
+    docs: "/",
+  },
+}
